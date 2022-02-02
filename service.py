fc4ca06321c11fe7846fceec51f6a8cf2ea16976
koutsilis1999 <kkoutsilis@gmail.com>
2022-02-02 22:01:15 +0000

    upload typehint

--- service.py
+++ service.py
@@ -1,6 +1,6 @@
-from fileinput import fileno
-from minio import Minio
 import os
+from minio import Minio
+from werkzeug.datastructures import FileStorage
 
 
 class StorageService:
@@ -15,7 +15,7 @@ class StorageService:
     def create_bucket(self, bucket_name: str) -> None:
         self.client.make_bucket(bucket_name=bucket_name)
 
-    def upload(self, bucket_name: str, file) -> dict:
+    def upload(self, bucket_name: str, file: FileStorage) -> dict:
         bucket_exists = self.client.bucket_exists(bucket_name)
         if not bucket_exists:
             self.create_bucket(bucket_name)
