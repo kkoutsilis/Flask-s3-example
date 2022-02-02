2c027e3ce2160edfbdde847567444de0eb0067f0
koutsilis1999 <kkoutsilis@gmail.com>
2022-02-02 22:01:59 +0000

    simple upload endpoint

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify
+from os import abort
+from flask import Flask
+from flask import jsonify, request
 from flask.typing import ResponseValue
+from service import StorageService
 
 app = Flask(__name__)
 
@@ -7,3 +10,13 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index() -> ResponseValue:
     return jsonify(message="Flask s3 example")
+
+
+@app.route("/upload", methods=["POST"])
+def upload() -> ResponseValue:
+    file = request.files.get("file")
+    if not file:
+        return jsonify(error="File required!"), 400
+    storage_service = StorageService()
+    res = storage_service.upload("default", file)
+    return jsonify(res)
